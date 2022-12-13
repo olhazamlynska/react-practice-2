9ba76ae26533ddcce50bd4638f3e6052e3fa9f6b
olhazamlynska <olya.sonechko@gmail.com>
2022-12-13 14:09:03 +0200

    Filles

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,27 @@
+export const Gallery = ({ movies, onDelete, openModal, changeWatched }) => {
+  return (
+    <>
+      <ul>
+        {movies.map(({ id, title, img, votes, watched }) => (
+          <li key={id}>
+            <h2>{title}</h2>
+            <p>Votes: {votes}</p>
+            <p>
+              Watched:
+              <span onClick={() => changeWatched(id)}>{String(watched)}</span>
+            </p>
+            <button type="button" onClick={() => onDelete(id)}>
+              Delete
+            </button>
+            <button
+              type="button"
+              onClick={() => openModal({ src: img, alt: title })}
+            >
+              Show poster
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
